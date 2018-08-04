7e210e8a18bdb48e75eaef884d046acb8402a3e6
rmuratzin <rmurtazin@4tifier.com>
2018-08-04 09:25:53 +0300

    validate and couple of pages added

---
+++ src/app/shared/services/common.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor() { }
+
+  validate(passwordFormGroup) {
+    const password = passwordFormGroup.value.password;
+    const repeatPassword = passwordFormGroup.value.password2;
+
+    if (repeatPassword.length <= 0) {
+      return null;
+    }
+
+    if (repeatPassword !== password) {
+      return {
+        doesMatchPassword: true
+      };
+    }
+    return null;
+  }
+
+  antiAdmin(control: FormControl): {[s: string]: boolean} {
+    const forbiddenName = ['admin'];
+    if (forbiddenName.indexOf(control.value.toLowerCase()) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+}
